4836c043830386ba908715c05cf043c1fa884f97
Dina Mostafa <dinamostafaghazalla@gmail.com>
2020-12-01 16:58:35 +0200

    Update app.module.ts

--- src/WebUI/ClientApp/src/app/app.module.ts
+++ src/WebUI/ClientApp/src/app/app.module.ts
@@ -8,8 +8,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
